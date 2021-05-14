cd0b87ced1a9498ba9999e8021170bf096806592
ikramkharbouch <4573r14@gmail.com>
2021-05-14 01:28:06 +0000

    feat: the whole project added and committed and pushed

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const about = document.querySelector('#about');
+const contact = document.querySelector('#contact');
+const aboutContent = document.querySelector('#about-content');
+const contactContent = document.querySelector('#contact-content');
+
+about.addEventListener('click', ()=> {
+   const aboutBox = new WinBox({
+       title: 'About Me',
+       background: '#00aa00',
+       modal: true,
+       width: '400px',
+       height: '400px',
+       top: 50,
+       right: 50,
+       bottom: 50,
+       left: 50,
+       mount: aboutContent,
+       onfocus: function() {
+           this.setBackground('#00aa00')
+       },
+       onblur: function() {
+        this.setBackground('#777')
+    }
+   })
+})
+
+contact.addEventListener('click', ()=> {
+    const contentBox = new WinBox({
+        title: 'Contact Me',
+        background: '#00aa00',
+        width: '400px',
+        height: '400px',
+        top: 150,
+        right: 50,
+        bottom: 50,
+        left: 250,
+        mount: contactContent,
+        onfocus: function() {
+            this.setBackground('#00aa00')
+        },
+        onblur: function() {
+            this.setBackground('#777')
+        }
+    })
+ })
